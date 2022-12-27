827129ddbc66f586db9897d14e9c43b1d41622a4
Kevin Everrett <everrett.developer@gmail.com>
2022-12-27 14:57:48 -0500

    Product Details

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai';
+import { GiMonkey } from 'react-icons/gi';
+import logo from '../public/assets/speaker1.webp';
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">E-Monkey Tech</Link>
+        <Link href="/">
+          <GiMonkey size={35} />
+        </Link>
       </p>
 
-      <button></button>
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
     </div>
   );
 };
